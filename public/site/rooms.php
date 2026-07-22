c85dfba0c5fa5abde246d4e0b4639d7f55b8f332
WizardOfXerox <wizardofxerox@users.noreply.github.com>
2026-07-23 01:00:09 +0800

    fix: add missing require_once room_showcase.php to public/site/rooms.php

--- public/site/rooms.php
+++ public/site/rooms.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/../includes/bootstrap.php';
 require_once __DIR__ . '/../includes/layout.php';
 require_once __DIR__ . '/../includes/room_catalog.php';
+require_once __DIR__ . '/../includes/room_showcase.php';
 
 $user = currentUser();
 $catalog = roomCatalog();
