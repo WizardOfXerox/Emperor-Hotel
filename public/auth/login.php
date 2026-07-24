006f1fb0d2dd414fa56b6c6db6035965b310888d
WizardOfXerox <wizardofxerox@users.noreply.github.com>
2026-07-25 01:15:44 +0800

    feat: add MAP navigation link to login and register header navigation bars

--- public/auth/login.php
+++ public/auth/login.php
@@ -49,6 +49,7 @@ renderHeader('Log In - Emperor Hotel', ['../assets/css/site/home.css'], '');
             <a class="home-nav__link" href="../site/home.php">HOME</a>
             <a class="home-nav__link" href="../site/rooms.php">ROOMS</a>
             <a class="home-nav__link" href="../site/suites.php">SUITES</a>
+            <a class="home-nav__link" href="../site/map.php">MAP</a>
             <a class="home-nav__link" href="../site/contact.php">CONTACT</a>
         </div>
 
